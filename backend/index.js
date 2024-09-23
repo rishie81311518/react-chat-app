2f1eb85dc78a02fe2744b4a73a1afffd7a22ff86
Rishi <rishiekandala@gmail.com>
2024-09-23 19:53:04 +0530

    Socket Backend setup

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import express from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/AuthRoutes.js";
 import contactsRoutes from "./routes/ContactRoutes.js";
+import setupSocket from "./socket.js";
 
 dotenv.config();
 
@@ -31,6 +32,8 @@ app.use("/api/contacts", contactsRoutes);
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+setupSocket(server)
 
 mongoose
   .connect(databaseURL)
